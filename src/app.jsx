403a93107591f09ccfabbec5889e404e6d78080c
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-12-04 17:46:13 -0600

    feat: create formField function to make each field dynamic and less repetitive

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,28 @@
-export default function App() {
-  return <h1 className="text-2xl font-bold underline">Hello, Vite + React!</h1>;
+function FormField({
+  value,
+  onChange,
+  label,
+  type = "text",
+  name,
+  id,
+  required = true,
+  validationMessage,
+}) {
+  return (
+    <div>
+      <label htmlFor="bookInfo" className="mb-1 block text-sm font-medium">
+        {label}
+      </label>
+      <input
+        id={id}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        className="w-full rounded border px-3 py-2"
+        required={required}
+      />
+      {validationMessage}
+    </div>
+  );
 }
